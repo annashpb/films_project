bea789d5bd1fc1531e1268000774b1420dfcf321
unknown <anna.sh.pb@gmail.com>
2022-09-12 19:56:50 +0300

    form completed

--- src/pages/FilmsPage/components/FilmForm.jsx
+++ src/pages/FilmsPage/components/FilmForm.jsx
@@ -7,13 +7,26 @@ const FilmForm = () => {
 	const [sel, setSel] = useState('');
 	const [multiSel, setMultiSel] = useState([]);
 
-	const handleTagsChange = () => {};
+	const handleTagsChange = (id) =>
+		setTags((v) =>
+			v.includes(id) ? v.filter((x) => x !== id) : [...v, id]
+		);
 
-	const handleGenreChange = () => {};
+	const handleGenreChange = (g) => setGenre(g);
 
-	const handleSelectChange = () => {};
+	const handleSelectChange = ({target}) => {
+		const {value} = target;
+		if(parseInt(value) === -1) {
+			alert('select option')
+			return;
+		}
+		setSel(value);
+	};
 
-	const handleMultiSelect = () => {};
+	const handleMultiSelect = ({ target }) => {
+		const multipleSelect = [...target.selectedOptions].map(o => o.value);
+		setMultiSel(multipleSelect);
+	};
 
 	return (
 		<form className="ui form">
@@ -22,33 +35,67 @@ const FilmForm = () => {
 					{/*  =========================  tags  ================  */}
 					<div className="grouped fields">
 						<label>Tags</label>
-						<div className="field">
-							<div className="ui checkbox field">
-								<input type="checkbox" id="tag1" />
-								<label htmlFor="tag1">Tag 1</label>
+						{tagsList?.map((tag) => (
+							<div className="field" key={`tags_${tag._id}`}>
+								<div className="ui checkbox field">
+									<input
+										type="checkbox"
+										id={`tags_${tag._id}`}
+										onChange={() =>
+											handleTagsChange(tag._id)
+										}
+										checked={tags.includes(tag._id)}
+									/>
+									<label htmlFor={`tags_${tag._id}`}>
+										{tag.title}
+									</label>
+								</div>
 							</div>
-						</div>
+						))}
 					</div>
 				</div>
 				{/*  ==============================   genre ================  */}
 				<div className="four wide column">
 					<div className="grouped fields">
 						<label>Genres</label>
-						{/* Start genres ====== */}
-						Genres here
-						{/* ====== finish genres  */}
+						{genres?.map((g) => (
+							<div
+								className="ui radio checkbox field"
+								key={`genres_${g._id}`}
+							>
+								<input
+									type="radio"
+									name="example2"
+									checked={genre === g._id}
+									id={`genres_${g._id}`}
+									onChange={() => handleGenreChange(g._id)}
+								/>
+								<label htmlFor={`genres_${g._id}`}>
+									{g.title}
+								</label>
+							</div>
+						))}
 					</div>
 				</div>
 				{/*  ==============================   sel ================  */}
 				<div className="four wide column">
-					<select className="ui dropdown">
-						<option value="1111">tag 1</option>
+					<select className="ui dropdown" value={sel} onChange={handleSelectChange}>
+						<option value="-1">Choose</option>
+						{genres?.map((g) => (
+							<option value={g._id} key={`sel_${g._id}`}>
+								{g.title}
+							</option>
+						))}
 					</select>
 				</div>
 				{/*  ==============================  multipleSelect ================  */}
 				<div className="four wide column">
-					<select multiple size="SIZE">
-						<option value="1111">tag 1</option>
+					<select multiple value={multiSel} onChange={handleMultiSelect} size={genres.length}>
+						{genres?.map((g) => (
+							<option value={g._id} key={`multisel_${g._id}`}>
+								{g.title}
+							</option>
+						))}
 					</select>
 				</div>
 			</div>
